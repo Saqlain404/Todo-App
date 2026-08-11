3f0a800aa81f4f4c383032e3c5dab482c9fd7fd6
Saqlain Malik <saqlainmalik73@gmail.com>
2026-08-12 05:13:41 +0530

    Fix backend render url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://todo-app-q5ju.onrender.com/api",
   withCredentials: true, // send + receive the httpOnly cookie
 });
 
